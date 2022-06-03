b6ad4c5fe3710234a088db170b5004750cb9fd5c
josh manansala <joshmanansalagsd@gmail.com>
2022-06-02 17:43:45 -0700

    footer section and menubutton

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("wah");
+"use strict";
+
+// navbar buttons variables
+const hamburger = document.querySelector("#open-btn");
+const closeBtn = document.querySelector("#close-btn");
+
+// navbar variables
+const navBar = document.querySelector(".navbar-nav");
+
+// navbar toggle function
+const navbarToggle = function () {
+  navBar.classList.toggle("active");
+};
+
+// added toggle func on navbar button
+hamburger.addEventListener("click", navbarToggle);
+closeBtn.addEventListener("click", navbarToggle);
